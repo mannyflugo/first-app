77f0dda0139bff72e03b2443f6bbbf1d92db7efa
mannyflugo <mannyflugo1@gmail.com>
2013-08-27 10:28:29 -0500

    new rails app for bloc heroku lesson

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '9246fd4aa23450de36230119698d78d8d5cebbae417ac7a68fd003bb692646ab02cdcf529df50d3bcc894a3298600533444ef8083782ca9b4488b46d6e3a4a26'
